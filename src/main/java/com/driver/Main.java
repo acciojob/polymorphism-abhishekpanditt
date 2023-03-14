b299ec30c1a635f7baf9903e12055974469662be
abhishekpanditt <badi.abhi@gmail.com>
2023-03-14 17:18:27 +0530

    Project updated

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -7,25 +7,29 @@ public class Main {
         Product p = new Product();
 
         int ans = p.product(30, 20);
+        System.out.println(ans);
 
         int ans1 = p.product(50, 20, 30);
+        System.out.println(ans1);
 
         double ans2 = p.product(10.5, 2.5);
-
-        System.out.println(ans + " " + ans1 + " " + ans2);
+        System.out.println(ans2);
     }
 
     public static class Product{
 
         public int product(int x, int y) {
+
             return x*y;
         }
 
         int product(int x, int y, int z) {
+
             return x*y*z;
         }
 
         public double product(double x, double y) {
+
             return x*y;
         }
     }
